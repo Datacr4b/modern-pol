457703cafa06b5c54ba26269ab00ae89269ff969
Datacrab <kamil.ejjennane@yahoo.com>
2026-02-11 18:54:42 +0100

    go back to browserrouter

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import { createRoot } from "react-dom/client";
 import App from "./components/app";
 import Question from "./components/question";
 import "./main.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Results from "./components/results";
 
 const domNode = document.querySelector("#root");
 const root = createRoot(domNode);
 
 root.render(
-    <HashRouter>
+    <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />} />
             <Route path="/test" element={<Question />} />
             <Route path="/results" element={<Results />} />
         </Routes>
-    </HashRouter>
+    </BrowserRouter>
 );
